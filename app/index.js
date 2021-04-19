6e525a864789392130a85fbe7e85cacce4326996
Lewis Liu <lewisl9029@users.noreply.github.com>
2021-04-19 02:19:54 +0000

    Add styling

--- app/index.js
+++ app/index.js
@@ -1,7 +1,16 @@
 import * as react from '@dep/react'
 import * as reactDom from '@dep/react-dom'
+import * as useStyles from '@dep/@lewisl9029/use-styles'
 
-const Root = () => react.createElement('div', undefined, 'working!')
+const Root = () => react.createElement(
+  useStyles.StylesProvider,
+  undefined, 
+  react.createElement(
+    'div', 
+    { className: useStyles.useStyles({ color: 'orange' }, [])}, 
+    'working if this text is orange'
+  )
+)
 
 const root = document.getElementById('root')
 
