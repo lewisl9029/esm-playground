31bbe5ee3fcd185239e4ff77321e3df13d2bd825
Lewis Liu <lewisl9029@users.noreply.github.com>
2021-04-19 02:42:16 +0000

    Fix mismatched react version

--- app/index.js
+++ app/index.js
@@ -2,14 +2,16 @@ import * as react from '@dep/react'
 import * as reactDom from '@dep/react-dom'
 import * as useStyles from '@dep/@lewisl9029/use-styles'
 
+const Test = () => react.createElement(
+  'div', 
+  { className: useStyles.useStyles({ color: 'orange' }, [])}, 
+  'working if this text is orange'
+)
+
 const Root = () => react.createElement(
   useStyles.StylesProvider,
   undefined, 
-  react.createElement(
-    'div', 
-    { className: useStyles.useStyles({ color: 'orange' }, [])}, 
-    'working if this text is orange'
-  )
+  Test
 )
 
 const root = document.getElementById('root')
